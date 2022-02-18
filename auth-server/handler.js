f9948a3748f3bd21d4329497ef61a9fc0358b21a
Justin Eastin <worldmisanthropy199@gmail.com>
2022-02-18 01:53:43 -0500

    typo and verification file
    add verification html from google

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -49,7 +49,7 @@ module.exports.getAuthURL = async () => {
   return {
     statusCode: 200,
     headers: {
-      "Access-Control-Allow-Origin": "*"
+      "Access-Control-Allow-Origin": "*",
     },
     body: JSON.stringify({
       authUrl: authUrl,
@@ -83,9 +83,6 @@ module.exports.getAccessToken = async (event) => {
       //respond with OAuth token
       return {
         statusCode: 200,
-        headers: {
-          'Access-Control-Allow-Origin': '*'
-        },
         body: JSON.stringify(token),
       };
     })
