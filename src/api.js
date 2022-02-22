685c342d89bb9e7468496b6f695b6c290ac59963
Justin Eastin <worldmisanthropy199@gmail.com>
2022-02-22 14:57:06 -0500

    testing
    testing functions Events and CitySearch

---
+++ src/api.js
@@ -0,0 +1,7 @@
+import React, { Components } from 'react';
+
+export const extractLocations = (events) => {
+    var extractLocations = events.map((event) => event.location);
+    var locations = [...new Set(extractLocations)];
+    return locations;
+};
