4cc108b6ea5b62489770d20974dd3c7a82f3ff64
Justin Eastin <worldmisanthropy199@gmail.com>
2022-02-22 21:40:07 -0500

    test fix
    showSuggestion fix

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -23,7 +23,8 @@ class CitySearch extends Component {
             query: suggestion,
             showSuggestions: false,
         });
-    }
+    };
+
     render() {
         return (
             <div className="CitySearch">
@@ -32,8 +33,11 @@ class CitySearch extends Component {
                     className="city"
                     value={this.state.query}
                     onChange={this.handleInputChanged}
+                    onFocus={() => {
+                        this.setState({ showSuggestions: true });
+                    }}
                 />
-                <ul className="suggestions">
+                <ul className="suggestions" style={this.state.showSuggestions ? {} : { display: "none" }}>
                     {this.state.suggestions.map((suggestion) => (
                         <li key={suggestion}
                         onClick={() => this.handleItemClicked(suggestion)}
