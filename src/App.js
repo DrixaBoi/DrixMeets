bc1f5170e2b5b6a0b7a1c88d97b4ef2f410ceb51
Justin Eastin <worldmisanthropy199@gmail.com>
2022-03-13 01:46:31 -0500

    css

--- src/App.js
+++ src/App.js
@@ -89,7 +89,9 @@ class App extends Component {
         </div>
         <WelcomeScreen showWelcomeScreen={this.state.showWelcomeScreen} getAccessToken={() => { getAccessToken() }} />
         <div className="header-items">
-          <EventGenre events={events}/>
+          <div className="pie-chart">
+            <EventGenre events={events}/>
+          </div>
           <CitySearch locations={locations} updateEvents={this.updateEvents} />
           <NumberOfEvents numberOfEvents={numberOfEvents} updateNumberOfEvents={this.updateNumberOfEvents} />
         </div>
